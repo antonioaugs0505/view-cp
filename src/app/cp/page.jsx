7bafe96d066423afcd7b999965eb4ee623382807
luanrdias23 <luan.rdias@gmail.com>
2023-11-09 22:07:02 -0300

    Design das paginas

--- src/app/cp/page.jsx
+++ src/app/cp/page.jsx
@@ -5,14 +5,19 @@ export default function CPage(){
 
     return(
         
-<div className="flex items-center justify-center">
-    <p className="text-center">
-        <Link href="/cp/user/lista">VIEW DO GET-ALL <FaTable /></Link>
+<div className="bg-purple-600 flex items-center justify-center h-screen">
+  <div className=" flex text-white text-center mb-24">
+    <p className="flex items-center border-4 rounded-3xl border-purple-800 px-5 py-2 m-5 hover:bg-purple-700">
+      <FaTable className="mr-5" />
+      <Link href="/cp/user/lista">VIEW DO GET-ALL</Link>
     </p>
 
-    <p className="text-center">
-        <Link href="/cp/user/detalhe/">VIEW DO GET-BY-ID <FaUser /></Link>
+    <p className="flex items-center border-4 rounded-3xl border-purple-800 px-5 py-2 m-5 hover:bg-purple-00">
+      <FaUser className="mr-5" />
+      <Link href="/cp/user/detalhe/">VIEW DO GET-BY-ID</Link>
     </p>
+  </div>
 </div>
+
     )
 }
